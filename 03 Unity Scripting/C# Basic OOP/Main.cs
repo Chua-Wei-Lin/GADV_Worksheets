c3fad04112b4f6792a9f5022e513245de09f8f43
Chua-Wei-Lin <2400680A@student.tp.edu.sg>
2025-05-22 17:20:50 +0800

    push

--- 03 Unity Scripting/C# Basic OOP/Main.cs
+++ 03 Unity Scripting/C# Basic OOP/Main.cs
@@ -1,23 +1,70 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    class Projectile
     {
-        Player player = new Player(10);
-        player.TakeDamage(3);
-        class Projectile 
+        private float speed;
+        public string Fire()
         {
-            private int Speed;
+            if(speed > 0)
+            {
+                return("Firing projectile a Speed: " + speed);
+            }
+            else
+            {
+                print("Cannot fire: speed too low.");
+                 return AutoFire();
+            }
+            
+        }
+        public string AutoFire()
+        {
+            speed = 100;
+            return("Speed was zero. AutoFire set speed to 100 and launched!");
+        }
+    }
+    public class Player
+    {
+        private int health;
 
-            speed = 1;
+        public int TakeDamage(int amount)
+        {
+            return health = health - amount;  
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+        public int Health()
+        {
+            return health = 100;
+        }
+    }
+    public class ScoreTracker
+    {
+        public int score;
+
+        public void ResetScore()
+        {
+            score = 0;
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    { 
+    
+        //Player player = new Player(10);
+        //player.TakeDamage(3);
         
+
     }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
 }
