025c8914ffe6fb6734b37c4c3ba32246a89e7a0b
Chua-Wei-Lin <2400680A@student.tp.edu.sg>
2025-07-16 20:50:09 +0800

    push

--- 06 Runtime Scripting, C# gernerics/Scripts/SpriteChanger.cs
+++ 06 Runtime Scripting, C# gernerics/Scripts/SpriteChanger.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class SpriteChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Sprite newSprite; // Assign this in the Inspector
+
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
-        
+        // Get the SpriteRenderer component from this GameObject
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Change the sprite if a new one is assigned
+        if (newSprite != null)
+        {
+            spriteRenderer.sprite = newSprite;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
